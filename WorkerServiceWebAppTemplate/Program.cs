f70344fe2a7f82092db807156ee1628f5d93c88f
Saeed Mohammadi <s.mohammadi@mabnadp.com>
2021-01-27 15:58:02 +0330

    Initialized

---
+++ WorkerServiceWebAppTemplate/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WorkerServiceWebAppTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostBuilderContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        }
+    }
+}
---
+++ WorkerServiceWebAppTemplate/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+namespace WorkerServiceWebAppTemplate
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("The service is alive!");
+                });
+            });
+        }
+    }
+}
